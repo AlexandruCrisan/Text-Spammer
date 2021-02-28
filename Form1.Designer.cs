99e38b5cd3799c3bed71a099bf0ea253d77f9b3a
Alexandru Crisan <calex2005cj@gmail.com>
2021-02-28 12:57:01 +0200

    adding / importing files

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿
+namespace Text_Spammer {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if ( disposing && (components != null) ) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.components = new System.ComponentModel.Container();
+            this.TB1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // TB1
+            // 
+            this.TB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.TB1.Location = new System.Drawing.Point(12, 72);
+            this.TB1.Multiline = true;
+            this.TB1.Name = "TB1";
+            this.TB1.Size = new System.Drawing.Size(290, 162);
+            this.TB1.TabIndex = 0;
+            this.TB1.TextChanged += new System.EventHandler(this.TB1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Black", 16F);
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 30);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text :";
+            // 
+            // btn_start
+            // 
+            this.btn_start.BackColor = System.Drawing.Color.LightGreen;
+            this.btn_start.Location = new System.Drawing.Point(21, 240);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(120, 57);
+            this.btn_start.TabIndex = 2;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = false;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.BackColor = System.Drawing.Color.LightGreen;
+            this.btn_stop.Location = new System.Drawing.Point(168, 240);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(120, 57);
+            this.btn_stop.TabIndex = 3;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = false;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.ClientSize = new System.Drawing.Size(318, 315);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TB1);
+            this.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.Name = "Form1";
+            this.Text = "Text Spammer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TB1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Text_Spammer {
+    public partial class Form1 : Form {
+        public Form1 () {
+            InitializeComponent();
+        }
+
+
+
+        private void Form1_Load (object sender, EventArgs e) {
+        }
+
+
+        public string user_input;
+
+        private void btn_start_Click (object sender, EventArgs e) {
+            timer1.Start();
+        }
+
+        private void btn_stop_Click (object sender, EventArgs e) {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick (object sender, EventArgs e) {
+            SendKeys.Send(user_input);
+            SendKeys.Send("{ENTER}");
+        }
+
+        private void TB1_TextChanged (object sender, EventArgs e) {
+            user_input = TB1.Text;
+        }
+    }
+}
